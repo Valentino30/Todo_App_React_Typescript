7c56a68543a7c344a3142dc2baa1042087c6ced3
Valentino <valentino@secret-source.eu>
2022-02-15 16:45:51 +0100

    Add user details to Dashboard container

--- src/containers/Dashboard/index.tsx
+++ src/containers/Dashboard/index.tsx
@@ -1,7 +1,19 @@
+import Text from '../../components/Text';
+import Button from '../../components/Button';
+import Header from '../../components/Header';
+
+import { useAuth } from '../../context/auth';
+
 export default function Dashboard() {
+  const { logout, user } = useAuth();
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <Button handleClick={logout}>Logout</Button>
+      <Header>Dashboard</Header>
+      <Text>{`id: ${user?.id}`}</Text>
+      <Text>{`email: ${user?.email}`}</Text>
+      <Text>{`token: ${user?.token}`}</Text>
     </div>
   );
 }
