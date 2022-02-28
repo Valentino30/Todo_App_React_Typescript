ce5228e4bf23c8332d9faf924168b0af631790bd
Valentino <valentino@secret-source.eu>
2022-02-28 16:48:25 +0100

    Add delay to apiCall util

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,8 +1,15 @@
 import { nanoid } from 'nanoid';
 
+const delay = (delay: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+};
+
 export const apiCall = {
   post: async (endpoint: string, data: Record<string, string>) => {
     console.info({ endpoint, data });
+    await delay(2000);
     const response = {
       data: {
         id: nanoid(),
